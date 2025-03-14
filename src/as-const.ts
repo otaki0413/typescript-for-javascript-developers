85fa64f30a7d13e0aebc1dcd8d9872554aca46f3
otaki0413 <takida0413@gmail.com>
2025-03-14 12:14:40 +0900

    constアサーションの学習

---
+++ src/as-const.ts
@@ -0,0 +1,16 @@
+export {};
+
+let name = "Kei";
+
+name = "Otaki";
+
+let nickname = "Otaki" as const;
+nickname = "Otaki";
+
+let profile = {
+  name: "Kei",
+  height: 185,
+} as const;
+
+// profile.name = "otaki";
+// profile.height = 190;
